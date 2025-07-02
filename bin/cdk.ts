e3f73fce55baec3046769c29b164b45b3b57f635
Olivier Andrade <olivier.andrade.mindera@guardian.co.uk>
2025-07-02 17:45:00 +0100

    ci tests for CDK

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { ManageHelpContentPublisherStack } from "../lib/manage-help-content-publisher-stack";
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { ManageHelpContentPublisherStack } from '../lib/manage-help-content-publisher-stack';
 
 const app = new App();
 
 // Deploy to different stages
-const stages = ["CODE", "PROD"];
+const stages = ['CODE', 'PROD'];
 
 stages.forEach((stage) => {
-  new ManageHelpContentPublisherStack(
-    app,
-    `ManageHelpContentPublisher-${stage}`,
-    {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
-      stage: stage,
-      stack: "membership",
-      app: "manage-help-content-publisher",
-    },
-  );
+	new ManageHelpContentPublisherStack(
+		app,
+		`ManageHelpContentPublisher-${stage}`,
+		{
+			env: {
+				account: process.env.CDK_DEFAULT_ACCOUNT,
+				region: process.env.CDK_DEFAULT_REGION,
+			},
+			stage: stage,
+			stack: 'membership',
+			app: 'manage-help-content-publisher',
+		},
+	);
 });
 
 app.synth();
